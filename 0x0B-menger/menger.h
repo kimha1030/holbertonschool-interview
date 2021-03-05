090e6492aeb8fda8c64bc7995d110dcc1f577665
kimha1030 <1478@holbertonschool.com>
2021-03-04 21:17:59 -0500

    Add header file

---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,12 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <math.h>
+
+int printSymbol(int x, int y);
+void menger(int level);
+
+#endif
