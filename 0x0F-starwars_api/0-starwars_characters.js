40ce56392a9c7248abb44fa39bd610f841750565
kimha1030 <1478@holbertonschool.com>
2021-04-07 15:49:04 -0500

    Change to async/await for work with promises

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,24 +1,27 @@
 #!/usr/bin/node
 
+const request = require("request");
 const idFilm = process.argv.slice(2, 3);
 const urlFilms = `https://swapi-api.hbtn.io/api/films/${idFilm}`;
-const request = require("request");
 const options = {
   url: urlFilms,
   method: "GET",
 };
 
-request(options, (err, res, body) => {
+request(options, async function (err, res, body) {
   if (err) {
     return console.log(err);
   }
   const results = JSON.parse(body).characters;
   for (const i in results) {
-    request(results[i], function (err, res, body) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(JSON.parse(body).name);
+    await new Promise((resolve, reject) => {
+      request(results[i], (err, res, body) => {
+        if (err) {
+          return console.log(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
   }
 });
