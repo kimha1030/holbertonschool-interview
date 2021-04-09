4704b2c92c486e1e0178c5958f4a0e8e4ed232f6
kimha1030 <1478@holbertonschool.com>
2021-04-08 23:28:16 -0500

    Fix syntax with semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 const idFilm = process.argv.slice(2, 3);
 const urlFilms = `https://swapi-api.hbtn.io/api/films/${idFilm}`;
 const options = {
   url: urlFilms,
-  method: "GET",
+  method: 'GET'
 };
 
 request(options, async function (err, res, body) {
