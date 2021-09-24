e1407ce425634facbc594f48f19b443d875d79db
kimha1030 <1478@holbertonschool.com>
2021-09-23 19:38:23 -0500

    Add header file

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void _puts(char *str);
+int _putchar(char c);
+void val_digit(char *str);
+int _isdigit(char str);
+char *mult(char *a, char *b, int l1, int l2);
+
+#endif
